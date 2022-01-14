46cc9f43e950d31fd3c584d8d9a80db3c555f23d
Reep236 <reep236bs@gmail.com>
2022-01-13 20:35:03 -0500

    More readable usage of datetime; avoid toggling of wysiSent/console spam

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import sys
 
 client = Client()
-WysiSent = False
+wysiSent = False
 
 try:
     configFn = sys.argv[1]
@@ -11,8 +11,8 @@ except:
     print("ERROR! Missing config file path!")
     exit(-1)
 
-with open(configFn, "r") as configFile:
-    config = configFile.read().split("\n")
+with open(configFn, "rt") as configFile:
+    config = configFile.readlines()
     clientToken = config[0]
     dmIDs = map(int, config[1:])
 
@@ -22,18 +22,18 @@ async def on_ready():
     print("We do a bit of trolling. (The script is connected to the client and is active)")
     print(f"Logged in as {client.user.name}")
     while 1:
-        if (datetime.now().hour == 19 or datetime.now().hour == 7) and datetime.now().minute == 27 and not WysiSent:
+        if not (datetime.now().time().strftime("%H:%M") in ["07:27", "19:27"]):
+            wysiSent = False
+        elif not wysiSent:
+            print("+=============================================================================================+")
+            print("| It is the current local time of the funny WYSI cookiezi funny blue zenith 727 funny number! |")
+            print(f"|                    Sending messages to user IDs listed in {configFn}...                     |")
+            print("+=============================================================================================+")
             for dm in dmIDs:
-                print("+=============================================================================================+")
-                print("| It is the current local time of the funny WYSI cookiezi funny blue zenith 727 funny number! |")
-                print(f"|                    Sending messages to user IDs listed in {configFn}...                     |")
-                print("+=============================================================================================+")
                 await (await client.fetch_user(dm)).send("WYSI")
                 await (await client.fetch_user(dm)).send("WYFSI")
                 await (await client.fetch_user(dm)).send("https://tenor.com/view/wysi-gif-21694798")
-            WysiSent = True
-        else:
-            WysiSent = False
+            wysiSent = True
 
 
 client.run(clientToken, bot=False)
